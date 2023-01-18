c2a16f58496ccacb99914bbc4e0b82617b7e591e
Laveez <niko.muukkonen@gofore.com>
2023-01-18 19:34:41 +0200

    Add automatic refresh, move events handling to backend

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import './App.css';
 import Events from './Events';
 import Lunch from './Lunch';
 import Sponsors from './Sponsors';
+import { useEffect } from 'react';
 
 export const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
+
+  // Automatically refresh the page every 1 hour
+  useEffect(() => {
+    setInterval(() => {
+      window.location.reload();
+    }, 3600000);
+  }, []);
+
   return (
     <div id="app">
       <Events />
--- src/Events.js
+++ src/Events.js
@@ -1,31 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import moment from 'moment/moment';
 import Spinner from './Spinner';
+import { API_URL } from './App';
 
 const Events = () => {
 
   const [ eventData, setEventData ] = useState();
 
   useEffect(() => {
-    fetch('https://clients6.google.com/calendar/v3/calendars/c_g2eqt2a7u1fc1pahe2o0ecm7as@group.calendar.google.com/' +
-      'events?calendarId=c_g2eqt2a7u1fc1pahe2o0ecm7as%40group.calendar.google.com&singleEvents=true' +
-      '&timeZone=Europe%2FHelsinki&maxAttendees=1&maxResults=250&sanitizeHtml=true' +
-      '&timeMin=2022-12-26T00%3A00%3A00%2B02%3A00&timeMax=2023-02-06T00%3A00%3A00%2B02%3A00' +
-      '&key=AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs')
+    fetch(API_URL + '/events')
         .then(response => response.json())
-        .then(data => setEventData(data));
+        .then(data => setEventData(data?.items));
   }, []);
-
-  const sortedEvents = eventData?.items.sort((a, b) => new Date(a.start.dateTime) - new Date(b.start.dateTime));
-  const currentEvents = sortedEvents?.filter(event => new Date(event.start.dateTime) > new Date());
 
   return (
     <div id="events">
       <h2>Tapahtumat</h2>
-      {!currentEvents && <Spinner size={40}/>}
+      {!eventData && <Spinner size={40}/>}
       <ul>
-        {currentEvents?.map(item =>
-          <li>{`[${moment(item.start.dateTime).format('DD.MM.YYYY')}] // ${item.summary}`}</li>
+        {eventData?.map(item =>
+          <li>
+            {`[${moment(item.start.dateTime).format('DD.MM.YYYY')}] // ${item.summary}`}
+          </li>
         )}
       </ul>
     </div>
