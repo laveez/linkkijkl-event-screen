afbe687e1bf2409adb599036101f05bfb28d5729
Laveez <niko.muukkonen@gofore.com>
2023-01-18 23:43:54 +0200

    Add functionality to hide the mouse after it has been stationary for 1 second

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import Events from './Events';
 import Lunch from './Lunch';
 import Sponsors from './Sponsors';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
+  const [cursorVisible, setCursorVisible] = useState(true);
 
   // Automatically refresh the page every 1 hour
   useEffect(() => {
@@ -15,8 +16,20 @@ function App() {
     }, 3600000);
   }, []);
 
+  // Hide mouse cursor when is has been stationary for 1 second
+  useEffect(() => {
+    let timeoutId;
+    const handleMouseMove = () => {
+      setCursorVisible(true);
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => setCursorVisible(false), 1000);
+    };
+    document.addEventListener('mousemove', handleMouseMove);
+    return () => document.removeEventListener('mousemove', handleMouseMove);
+  }, []);
+
   return (
-    <div id="app">
+    <div id="app" className={cursorVisible ? 'cursor-visible' : 'cursor-hidden'}>
       <Events />
       <Lunch />
       <Sponsors />
